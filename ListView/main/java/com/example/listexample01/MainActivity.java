e13cfdb43a300d6da4189d73718a21e129116159
kjm232311 <kjm232311@gmail.com>
2023-07-11 22:34:00 +0900

    Add files via upload

---
+++ ListView/main/java/com/example/listexample01/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.listexample01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = (ListView)findViewById(R.id.list);
+
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,data);
+        list.setAdapter(adapter);
+
+        data.add("달콤한잼");
+        data.add("콤잼");
+        data.add("애플");
+        adapter.notifyDataSetChanged();
+
+    }
+}
